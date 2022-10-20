7d2f750513bd4519f402fd86178913e7d5715e3c
rajesh gangoni <rajesh.gangoni@berrybytes.com>
2022-10-20 11:52:55 +0530

    Added groovy file

---
+++ vars/sendNotifications.groovy
@@ -0,0 +1,18 @@
+def call() {
+  
+ 
+  def subject = "Notification"
+  def summary = "${subject} (${env.BUILD_URL})"
+  def details = 'SonarQube result URL: ${BUILD_LOG_REGEX, regex=".*ANALYSIS SUCCESSFUL, you can browse (.*)", showTruncatedLines=false, substText="$1"}'
+
+   //emailext body: 'SonarQube result URL: ${BUILD_LOG_REGEX, regex=".*ANALYSIS SUCCESSFUL, you can browse (.*)", showTruncatedLines=false, substText="$1"}', mimeType: 'text/html', subject: 'Notification', to: 'rajesh.gangoni@sunrise.net , rajeshgangoni@gmail.com'
+              // } 
+
+  emailext (
+      to: 'rajesh.gangoni@sunrise.net , rajeshgangoni@gmail.com',
+      mimeType: 'text/html',
+      subject: subject,
+      body: details
+      //recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+    )
+}
