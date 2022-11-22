01312f6dfd2a8e6ab6d1d524d7e9b29df8fa4a57
rajeshgangoni <43514123+rajeshgangoni@users.noreply.github.com>
2022-11-22 16:13:11 +0530

    Update sendNotifications.groovy

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -9,7 +9,7 @@ def call() {
               // } 
 
   emailext (
-      to: 'rajesh.gangoni@sunrise.net , rajeshgangoni@gmail.com',
+      //to: 'rajesh.gangoni@sunrise.net , rajeshgangoni@gmail.com',
       mimeType: 'text/html',
       subject: subject,
       body: details
